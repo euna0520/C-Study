2c9190da788ec7e2edb63800446f54a72951e1dd
euna <45058268+euna0520@users.noreply.github.com>
2018-12-07 19:22:30 +0900

    study3

---
+++ Study03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study03
+{
+    public struct MyStruct
+    {
+        public const float PI = 3.14f;
+        public static int Age = 12;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", MyStruct.PI, MyStruct.Age);
+
+            Console.ReadKey();
+        }
+    }
+}
